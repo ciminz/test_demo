e782a61e03a90bec9d7a159c792f63178d9d175e
zhouzhiming <zzming0571@163.com>
2019-11-26 19:10:50 +0800

    20191126update

--- test_demo/src/main/java/com/example/demo/TestDemoApplication.java
+++ test_demo/src/main/java/com/example/demo/TestDemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
 		SpringApplication.run(TestDemoApplication.class, args);
 	}
 
